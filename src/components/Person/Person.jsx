18ca66e60ad20a0d46cbfa576da0d1eb69cb699e
Danil Dzirun <danil.dzirun1486@gmail.com>
2025-05-13 18:17:39 +0300

    Fixed issues

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,7 +16,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {age !== null && <p className="Person__age">{`I am ${age}`}</p>}
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
 
       {isMarried === true ? (
         sexOfPartner
